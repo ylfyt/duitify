a597fc4e2a7d871ee65ef126e724035c81b5e82e
ylfyt <yalfayat@gmail.com>
2024-11-29 10:47:01 +0700

    wip: adding pwa

--- vite.config.ts
+++ vite.config.ts
@@ -2,18 +2,54 @@ import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import path from 'path';
 import Icons from 'unplugin-icons/vite';
+import { VitePWA } from 'vite-plugin-pwa';
+import { COLOR_SCHEMES } from './src/constants/color-schemes';
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
     process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
 
+    const BASE_URL = process.env.NODE_ENV === 'production' ? '/duitify' : '';
+
     return {
-        plugins: [react(), Icons({ compiler: 'jsx', jsx: 'react' })],
+        plugins: [
+            react(),
+            Icons({ compiler: 'jsx', jsx: 'react' }),
+            VitePWA({
+                registerType: 'autoUpdate',
+                devOptions: {
+                    enabled: true,
+                },
+                manifest: {
+                    name: 'Duitify',
+                    short_name: 'Duitify',
+                    start_url: '/duitify/',
+                    scope: 'http://192.168.0.201:4173/duitify/',
+                    description: 'Manage your finances easily',
+                    theme_color: COLOR_SCHEMES.light.primary,
+                    display: 'standalone',
+                    icons: [
+                        {
+                            src: BASE_URL + '/icons/icon-192x192.png',
+                            sizes: '192x192',
+                            type: 'image/png',
+                        },
+                        {
+                            src: BASE_URL + '/icons/icon-512x512.png',
+                            sizes: '512x512',
+                            type: 'image/png',
+                        },
+                    ],
+                },
+                workbox: {
+                    globPatterns: ['**/*.{js,css,html,ico,png,svg,webp,xml,ttf,otf,woff,woff2}'],
+                },
+            }),
+        ],
         server: {
             host: '0.0.0.0',
             port: 4000,
         },
-        base: process.env.NODE_ENV === 'production' ? '/duitify' : '/',
+        base: BASE_URL,
         resolve: {
             alias: {
                 '@': path.resolve(__dirname, 'src'),
